7680f8c1ef23f922121d1cbb2d3daa1963325257
Fluxpoint <fluxitegmd@gmail.com>
2026-03-06 17:09:06 +0100

    Update LevelAuthors.js

--- js/components/List/LevelAuthors.js
+++ js/components/List/LevelAuthors.js
@@ -18,7 +18,7 @@ export default {
             <template v-if="selfVerified">
                 <div class="type-title-sm">Creator & Verifier</div>
                 <p class="type-body">
-                    <span>{{ author }}</span>
+                    <a @click.prevent="$emit('goToLeaderboard', author)" href="#">{{ author }}</a>
                 </p>
             </template>
             <template v-else-if="creators.length === 0">
@@ -28,20 +28,19 @@ export default {
                 </p>
                 <div class="type-title-sm">Verifier</div>
                 <p class="type-body">
-                    <span>{{ verifier }}</span>
+                    <a @click.prevent="$emit('goToLeaderboard', verifier)" href="#">{{ verifier }}</a>
                 </p>
             </template>
             <template v-else>
                 <div class="type-title-sm">Creators</div>
                 <p class="type-body">
-                    <template v-for="(creator, index) in creators" :key="\`creator-\$\{creator\}\`">
-                        <span >{{ creator }}</span
-                        ><span v-if="index < creators.length - 1">, </span>
+                    <template v-for="(creator, index) in creators" :key="\`creator-\${creator}\`">
+                        <span>{{ creator }}</span><span v-if="index < creators.length - 1">, </span>
                     </template>
                 </p>
                 <div class="type-title-sm">Verifier</div>
                 <p class="type-body">
-                    <span>{{ verifier }}</span>
+                    <a @click.prevent="$emit('goToLeaderboard', verifier)" href="#">{{ verifier }}</a>
                 </p>
             </template>
             <div class="type-title-sm">Publisher</div>
@@ -50,6 +49,8 @@ export default {
             </p>
         </div>
     `,
+
+    emits: ['goToLeaderboard'],
 
     computed: {
         selfVerified() {
